cf07d1c3edacdaac82e22d12eea6773db02036e2
emilmarcuarez <emilmarpatricia@gmail.com>
2024-10-10 00:03:58 -0400

    notificaciones listas en otra pagina

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NotificacionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VacanteController;
@@ -19,5 +20,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// notificaciones. No se especifica nada porque es invoke, solo usa eso
+Route::get('/notificaciones', NotificacionController::class);
 
 require __DIR__.'/auth.php';
